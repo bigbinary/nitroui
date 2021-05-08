284354fd924712e8ee5d26b5c62a34ba491c8586
Edwin Babu <edwinbbu@gmail.com>
2021-05-08 16:06:08 +0530

    reverse testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  // important: true,
+  important: true,
   purge: {
     enabled: process.env.NODE_ENV === "production" ? true : false,
     content: [
